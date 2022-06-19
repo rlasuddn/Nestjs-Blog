b4c3e14ce6f2b0fb6c1bfa2f03556360733c08a6
rlasuddn <rlasuddn>
2022-06-19 22:38:13 +0900

    fix(exceotion):모든 예외처리 필터 추가+서비스 레포지토리 로직 역할 변경

--- src/http-exception.filter.ts
+++ src/http-exception.filter.ts
@@ -1,4 +1,5 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus, InternalServerErrorException } from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
 import { Request, Response } from 'express';
 
 @Catch(HttpException)
@@ -9,7 +10,8 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
     const error = exception.getResponse() as string | { error: string; statusCode: number; message: string | string[] };
-
+    console.log(error);
+    // console.log(typeof error);
     if (typeof error === 'string') {
       response.status(status).json({
         success: false,
@@ -24,5 +26,24 @@ export class HttpExceptionFilter implements ExceptionFilter {
         ...error,
       });
     }
+  }
+}
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: InternalServerErrorException, host: ArgumentsHost): void {
+    // In certain situations `httpAdapter` might not be available in the
+    // constructor method, thus we should resolve it here.
+    const ctx = host.switchToHttp();
+    const httpStatus = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+    response.status(httpStatus).json({
+      success: false,
+      statusCode: httpStatus,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message: exception.message,
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './http-exception.filter';
+import { AllExceptionsFilter, HttpExceptionFilter } from './http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(new HttpExceptionFilter(), new AllExceptionsFilter());
   await app.listen(3000);
   console.log('http://localhost:3000');
 }
